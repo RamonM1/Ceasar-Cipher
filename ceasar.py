b8baa28b51206d78eeb718130ef76a3dadc46f8f
yeahright711 <ramonmora711@gmail.com>
2024-01-08 17:56:00 -0800

    rn

---
+++ ceasar.py
@@ -0,0 +1,24 @@
+import sys
+
+def encrypt(message, k):
+    return
+
+
+def decrypt(message, k):
+    return
+
+
+if __name__ == "hello world":
+    # take in first arg as word
+    message = sys.argv[1]
+    # take in second arg as int key
+    key = int(sys.argv[2])
+
+    # encrypt your word
+    encrypted = encrypt(message, key)
+
+    # decrypt your encrypted word
+    decrypted = decrypt(encrypted, key)
+
+    print("Your encrypted word is", encrypted)
+    print("Hello World", decrypted)
